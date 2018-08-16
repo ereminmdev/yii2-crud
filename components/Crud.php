4ab6508324bd57ae1574c7b322d35e54f07bdfc3
Eremin Michael <misha@ut6.ru>
2018-08-16 18:57:59 +0500

    Improve to get all attributes from model for export

--- components/Crud.php
+++ components/Crud.php
@@ -1062,7 +1062,7 @@ $(".js-checked-action").on("click", function () {
             'renderData' => (bool)$model->renderData,
             'model' => $this->getModel('search'),
             'dataProvider' => $this->getDataProvider(),
-            'columns' => $this->getConfig('exportColumns', $this->guessColumns($this->getFields())),
+            'columns' => $this->getConfig('exportColumns', $this->getModel()->getAttributes()),
         ]);
         return $exporter->export();
     }
