e24b352ff50cb2cfffcd614561830599a441a577
Eremin Mikhail <ereminmdev@gmail.com>
2020-11-30 18:57:37 +0500

    Add icons to submit and delete form buttons

--- views/default/_form.php
+++ views/default/_form.php
@@ -30,15 +30,15 @@ $controller = $this->context;
     <hr>
 
     <div class="form-group form-buttons">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('crud', 'Create') : Yii::t('crud', 'Update'),
+        <?= Html::submitButton('<span class="glyphicon glyphicon-ok"></span> ' . ($model->isNewRecord ? Yii::t('crud', 'Create') : Yii::t('crud', 'Update')),
             ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         &nbsp;
         <?= Html::a(Yii::t('crud', 'Cancel'), $controller->getReturnUrl(), ['class' => 'btn btn-link', 'onclick' => 'window.history.back(); return false']) ?>
 
         <?php if (!$model->isNewRecord && $crud->getConfig('access.delete', true)): ?>
             <div class="pull-right">
-                <?= Html::a(Yii::t('crud', 'Delete'), $controller->urlCreate(['delete', 'id' => $model->getPrimaryKey()]),
-                    ['class' => 'btn btn-default', 'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'), 'data-form' => 'delete record']) ?>
+                <?= Html::a('<span class="glyphicon glyphicon-remove"></span> ' . Yii::t('crud', 'Delete'), $controller->urlCreate(['delete', 'id' => $model->getPrimaryKey()]),
+                    ['class' => 'btn btn-danger', 'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'), 'data-form' => 'delete record']) ?>
             </div>
         <?php endif; ?>
     </div>
