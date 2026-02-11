2a0da1cee86ea762e2498e6c75e3a8e000f06b61
Eremin Mikhail <ereminmdev@gmail.com>
2026-02-11 14:05:45 +0500

    Restore labelHint config param

--- components/Crud.php
+++ components/Crud.php
@@ -738,6 +738,14 @@ class Crud extends BaseObject
                 }
 
                 if (isset($columns[$key]) && is_array($columns[$key])) {
+                    if (isset($schema['labelHint'])) {
+                        $columns[$key]['label'] = ($columns[$key]['label'] ?? Html::encode($model->getAttributeLabel($field)));
+                        if ($columns[$key]['encodeLabel'] ?? true) {
+                            $columns[$key]['label'] = Html::encode($columns[$key]['label']);
+                        }
+                        $columns[$key]['label'] = $columns[$key]['label'] . ' <span class="glyphicon glyphicon-question-sign text-muted" title="' . Html::encode($schema['labelHint']) . '"></span>';
+                        $columns[$key]['encodeLabel'] = false;
+                    }
                     if (isset($schema['gridColumnOptions'])) {
                         $columns[$key] = ArrayHelper::merge($columns[$key], (array)$schema['gridColumnOptions']);
                     }
